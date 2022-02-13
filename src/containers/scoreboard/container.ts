9d54a46f80ba879a9739d6ac3d9b48de4ffc6f82
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-02-12 21:00:02 -0600

    add scoreboard container

---
+++ src/containers/scoreboard/container.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scoreboard',
+  templateUrl: './scoreboard.container.html',
+  styleUrls: ['./scoreboard.container.scss']
+})
+export class ScoreboardContainer {
+
+}
