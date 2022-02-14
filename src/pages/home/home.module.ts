3e135d621c0aa1fc10b442213b638d98df90c2f2
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-02-13 19:59:43 -0600

    finish home page

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
+import { ButtonModule } from 'src/components/button/button.module';
 
 const routes: Routes = [
   {
@@ -13,7 +14,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ButtonModule
   ],
   declarations: [
     HomePage
