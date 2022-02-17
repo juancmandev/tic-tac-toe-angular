9a6fd7a58e3091b7edbb9fe8289532445302e112
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-02-16 23:09:51 -0600

    add firebase configuration, fix ng build bug and deploy app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-HDCLPCrMX_IjOQONLEpLNmThqZ4HqjE",
+  authDomain: "tic-tac-toe-angular-28723.firebaseapp.com",
+  projectId: "tic-tac-toe-angular-28723",
+  storageBucket: "tic-tac-toe-angular-28723.appspot.com",
+  messagingSenderId: "838851477450",
+  appId: "1:838851477450:web:d2b05945b85cc36c575933",
+  measurementId: "G-F3QZHMSD6S"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +32,14 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
